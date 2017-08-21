527331c03a40199eceef0b1d5ca0835bcacd6952
StevenZhao13 <zhaoyuzhengze@gmail.com>
2017-08-21 15:29:03 -0500

    all set, waiting for testing

--- ConsoleApp1/GameCore.cs
+++ ConsoleApp1/GameCore.cs
@@ -8,13 +8,13 @@ namespace ConsoleApp1
     {
 
         public int proximityCount;
-        public bool isBomb;
+        public bool isNade;
         public bool isVisible;
 
         public Square(bool isBomb)
         {
             this.proximityCount = 0;
-            this.isBomb = isBomb;
+            this.isNade = isBomb;
             this.isVisible = false;
         }
     }
@@ -23,6 +23,9 @@ namespace ConsoleApp1
     class GameCore
     {
         private Square[][] minefield;
+
+        public static int GAME_STATE = 0, LOST_STATE = 1, WIN_STATE = 2;
+        public int state;
 
         public GameCore(int size, int percentage)
         {
@@ -35,7 +38,6 @@ namespace ConsoleApp1
                 this.minefield[i] = tempColumn;
 
                 for ( int j = 0; j < this.minefield.Length; j++ ) {
-
                     Square curSquare;
 
                     int roll = randomNumberGen.Next(1,100);
@@ -45,7 +47,10 @@ namespace ConsoleApp1
                     this.minefield[i][j] = curSquare;
                 }
             }
+
+            this.state = GameCore.GAME_STATE;
         }
+        // End constructor func
 
 
         /*
@@ -60,6 +65,7 @@ namespace ConsoleApp1
                 return null;
             }
         }
+        // End func
 
 
         /*
@@ -68,7 +74,7 @@ namespace ConsoleApp1
         {
             for ( int i = 0; i < this.minefield.Length; i++ ) {
                 for ( int j = 0; j < this.minefield.Length; j++ ) {
-                    if ( this.getSpecifiedSquare(j, i).isBomb == true ) {
+                    if ( this.getSpecifiedSquare(j, i).isNade == true ) {
                         Square tempPointer;
                         // Top left
                         tempPointer = this.getSpecifiedSquare(j - 1, i - 1);
@@ -95,11 +101,12 @@ namespace ConsoleApp1
                         tempPointer = this.getSpecifiedSquare(j + 1, i + 1);
                         if ( tempPointer != null ) tempPointer.proximityCount++;
 
-                    } else { }
+                    } else {
+                    }
                 }
             }
         }
-
+        // End func
 
 
         /*
@@ -113,13 +120,11 @@ namespace ConsoleApp1
                 for ( int j = 0; j < this.minefield.Length; j++ ) {
 
                     if ( this.minefield[i][j].isVisible ) {
-                        if ( this.minefield[i][j].isBomb ) {
+                        if ( this.minefield[i][j].isNade ) {
                             Console.Write("*");
                         } else {
                             Console.Write(this.minefield[i][j].proximityCount);
                         }
-
-
 
                     } else {
                         Console.Write("?");
@@ -128,5 +133,111 @@ namespace ConsoleApp1
                 Console.Write("\n");
             }
         }
+        // End Func
+
+
+        /*
+         * RIP method.
+         */
+        public void NadeTripped()
+        {
+            this.state = GameCore.LOST_STATE;
+            for ( int i = 0; i < this.minefield.Length; i++ ) {
+                for ( int j = 0; j < this.minefield.Length; j++ ) {
+                    this.minefield[i][j].isVisible = true;
+                }
+            }
+        }
+        // End Func
+
+
+        /*
+         */
+        public void updateGameState()
+        {
+            bool isEnded = true;
+            for ( int i = 0; i < this.minefield.Length && isEnded; i++ ) {
+                for ( int j = 0; j < this.minefield.Length && isEnded; j++ ) {
+                    if ( this.minefield[i][j].isNade == false
+                        && this.minefield[i][j].isVisible == false ) {
+                        isEnded = false;
+                    } else {
+                    }
+                }
+            }
+
+        }
+        // End Func
+
+
+        /*
+         * This is a self-contained function that read & parse player input from console
+         * And further make the change to 
+        */
+        public void readPlayerInupt()
+        {
+            Console.WriteLine("Plz input your selection of place thru the following " +
+                "format: x y");
+
+            bool inputParsed = false;
+            while ( !inputParsed ) {
+                String inputLine = Console.ReadLine();
+                String[] splitedInput = inputLine.Split();
+
+                int x, y;
+                if ( splitedInput.Length == 2
+                    && int.TryParse(splitedInput[0], out x)
+                    && int.TryParse(splitedInput[0], out y)
+                    && this.getSpecifiedSquare(x - 1, y - 1) != null ) {
+
+                    Square targetSq = this.getSpecifiedSquare(x, y);
+                    if ( targetSq.isNade ) {
+                        // RIP
+                        this.NadeTripped();
+                    } else {
+                        targetSq.isVisible = true;
+                    }
+
+                    inputParsed = true;
+                } else {
+                }
+            }
+        }
+        // End Func
+
+
+        /*
+         */
+        public void mainProcess()
+        {
+            while ( this.state == GameCore.GAME_STATE ) {
+                this.readPlayerInupt();
+                this.updateGameState();
+            }
+
+            if ( this.state == GameCore.LOST_STATE ) {
+                Console.WriteLine("Get rekt");
+            } else if ( this.state == GameCore.WIN_STATE ) {
+                Console.WriteLine("You are pretty good");
+            } else {
+                Console.WriteLine("Something went wrnog");
+            }
+        }
+        // End Func
+
+
+        /*
+         * 
+         * Main Func
+         */ 
+        public static void Main(String[] args)
+        {
+            GameCore inst = new GameCore(20, 20);
+
+        }
+
     }
+
+
+
 }
